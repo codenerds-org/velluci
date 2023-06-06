c2dd60ac5dac5f4d7390eb6e2c10936a2e0b7b50
style77 <stylek777@gmail.com>
2023-06-06 17:25:36 +0200

    chore: change footer text to specify its demo page

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="flex flex-row items-center justify-center gap-8">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-2xl">Velluci</h1>
-          <p className="text-sm">Street-style luxury</p>
+          <p className="text-sm">Demo properties website</p>
         </div>
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-2xl">Contact</h1>
@@ -15,7 +15,7 @@ const Footer = () => {
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-2xl">Social</h1>
           <p className="text-sm">
-            <a href="https://www.instagram.com/velluci_official/">Instagram</a>
+            <a href="https://codenerds.tech/">Instagram</a>
           </p>
         </div>
       </div>
