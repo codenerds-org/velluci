99cc0d6473cff971f32cd4335f44570ae311fce8
style77 <stylek777@gmail.com>
2023-06-06 17:25:51 +0200

    chore: improve cart item

--- src/app/components/CartItem.tsx
+++ src/app/components/CartItem.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import { FaTrash } from "react-icons/fa";
 import { Product } from "@/app/types/product";
-import { CartEntry } from "use-shopping-cart/core";
+import { CartEntry, formatCurrencyString } from "use-shopping-cart/core";
+import { useShoppingCart } from "use-shopping-cart";
+import config from "@/../config.json";
 
 export default function CartItem(item: CartEntry) {
-  const { name, price, image } = item;
+  const { removeItem } = useShoppingCart();
+  const { name, price, currency, image } = item;
+
 
   return (
     <div className="flex items-center gap-4 mb-3">
-      <div>{name}</div>
-      <div className="ml-auto">{price}</div>
-      <button className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1">
+      <div className="flex flex-row gap-2 items-center">
+        <Image src={image!} alt={name} width={100} height={100} className="h-12 w-8" />
+        {name}
+      </div>
+      <div className="ml-auto">{formatCurrencyString({ value: price, currency: currency, language: config.locale })} x {item.quantity}</div>
+      <button className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1 flex items-center" onClick={() => removeItem(item.id)}>
         <FaTrash />
       </button>
     </div>
